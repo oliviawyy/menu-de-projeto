ceb1c639cce54ef72bf86a985ca88feeee5f9500
oliviawyy <olivia.c@aluno.senai.br>
2026-02-26 11:19:38 -0300

    fazendo o evento de click, e o arrow

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// 1. lógica do menu principal (index, obter os elementos)
+const btnP1 = document.querySelector('#inputP1');
+const btnP2 = document.querySelector('#inputP2');
+const btnP3 = document.querySelector('#inputP3');
+
+// 2. método addEventListener com uma função anonima
+
+btnP1.addEventListener('click', () => {
+    window.location.href='./pages/projeto01.html'
+});
+
+btnP2.addEventListener('click', function abrirP2(){
+    window.location.href='./pages/projeto02.html'
+});
+
+btnP3.addEventListener('click', function abrirP3(){
+    window.location.href='./pages/projeto03.html'
+});
+
+function cacho(x,y){
+    return x + y
+};
+
+const banana = cacho
+
+
+// Arrow Function '=>' tipo um return so que em seta de forma simplificada
+const uva = () => 'Sou uma fruta';
