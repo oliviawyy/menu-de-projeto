5deb628e82c25072085fcd38c6d94f3ecdab286f
oliviawyy <olivia.c@aluno.senai.br>
2026-03-05 10:57:02 -0300

    acabou

--- js/global-auto.js
+++ js/global-auto.js
@@ -10,7 +10,29 @@ if (botoesProjeto.length > 0) {
             // O index começa em 0, então somamos 1 para bater com "projeto01", "projeto02"...
             const numeroProjeto = (index + 1).toString().padStart(2, '0');
             alert(numeroProjeto)
-            window.location.href = `./pages/projetos${numeroProjeto}.html`;
+            window.location.href = `./pages/projeto${numeroProjeto}.html`;
         });
     });
 };
+
+const criarBotaoVoltar = () => {
+    const btnVoltar = document.createElement('button');
+    btnVoltar.innerText = 'Voltar';
+    
+    // herdará o estilo de 'button' do css
+    btnVoltar.className = 'btn-navegacao';
+
+    btnVoltar.addEventListener('click', () => {
+        window.location.href = '../index.html';
+    });
+
+    document.body.appendChild(btnVoltar);
+}
+
+if (botoesProjeto.length === 0) {
+    // adicionando um evento no v=navegador (objeto window).
+    // Após o carregamento da página a função sera invocada()
+    window.addEventListener('load', () => {
+        criarBotaoVoltar();
+    })
+}
